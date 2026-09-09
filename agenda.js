76bdfa7cff5be58fd16f6957b2ba8554788c3a7b
ZanettiPedro1 <zanettipedro2712@gmail.com>
2026-09-08 23:27:02 -0400

    Improve error message when schedule is occupied
    Update error message for occupied schedule

--- agenda.js
+++ agenda.js
@@ -79,8 +79,7 @@ formulario.addEventListener("submit", (evento) => {
   const consultas = carregar();
 
   if (horarioOcupado(consultas, nova)) {
-    mensagem.textContent = "erro";
-    formulario.reset();
+  mensagem.textContent = `O horário ${nova.hora} já está ocupado com ${nova.profissional}. Escolha outro horário.`;
     return;
   }
 
